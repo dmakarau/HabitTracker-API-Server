9b0785a31ebe8634d0a22f254a262d8f8b7a464f
dmakarau <denis.makarow@gmail.com>
2025-10-10 16:48:33 +0200

    Started creating item migration table

--- Sources/GrowBitAppServer/Migrations/CreateItemsTableMigration.swift
+++ Sources/GrowBitAppServer/Migrations/CreateItemsTableMigration.swift
@@ -1,5 +1,5 @@
 //
-//  CreateHabitsTableMigration.swift
+//  CreateItemsTableMigration.swift
 //  GrowBitAppServer
 //
 //  Created by Denis Makarau on 10.10.25.
@@ -8,10 +8,18 @@
 import Foundation
 import Fluent
 
-struct CreateHabitsTableMigration: AsyncMigration {
+struct CreateItemsTableMigration: AsyncMigration {
     
     func prepare(on database: any Database) async throws {
-        try await database.schema
+        try await database.schema("items")
+            .id()
+            .field("title", .string, .required)
+            .field("description", .string)
+            .field("start_data", .date, .required)
+//            .field("frequency", .enum(type: "item_frequency"), .required)
+            .field("category_id", .uuid, .required, .references("habits_categories", "id", onDelete: .cascade))
+            .create()
+        
     }
     
     func revert(on database: any Database) async throws {
