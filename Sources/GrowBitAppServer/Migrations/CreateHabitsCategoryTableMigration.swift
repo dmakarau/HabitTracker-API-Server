b3e05820ddfe89b173ed9868c6ed53e922569702
dmakarau <denis.makarow@gmail.com>
2025-10-24 16:40:48 +0200

    renamed schemas

--- Sources/GrowBitAppServer/Migrations/CreateHabitsCategoryTableMigration.swift
+++ Sources/GrowBitAppServer/Migrations/CreateHabitsCategoryTableMigration.swift
@@ -11,7 +11,7 @@ import Fluent
 struct CreateHabitsCategoryTableMigration: AsyncMigration {
     
     func prepare(on database: any Database) async throws {
-        try await database.schema("habits_categories")
+        try await database.schema("categories")
             .id()
             .field("name", .string, .required)
             .field("color_code", .string, .required)
@@ -20,7 +20,7 @@ struct CreateHabitsCategoryTableMigration: AsyncMigration {
     }
     
     func revert(on database: any Database) async throws {
-        try await database.schema("habits_categories")
+        try await database.schema("categories")
             .delete()
     }
 }
